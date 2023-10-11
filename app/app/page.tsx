bd98a1c154bf0eceed57fe2ebd786c176baf4226
Pumidol Leelerdsakulvong <pumidol.l@gmail.com>
2023-10-11 18:18:41 +0000

    feat: temp dashboard

--- app/app/page.tsx
+++ app/app/page.tsx
@@ -5,7 +5,22 @@ import { getServerSession } from "next-auth"
 
 export default async function Page() {
   const session = await getServerSession(authOptions)
+  if (!session) return (
+    <div className="h-screen flex items-center">
+      <div className="container mx-auto h-3/4">
+        <div className="flex justify-center">
+        <AuthBtn callbackUrl="/" />
+        </div>
+      </div>
+    </div>
+  )
   return (
-    <AuthBtn />
+    <div className="h-screen flex items-center">
+      <div className="container mx-auto h-3/4">
+        <p className="text-center">
+          Hello, {session.user.name} select an App to get started.
+        </p>
+      </div>
+    </div>
   )
 }
