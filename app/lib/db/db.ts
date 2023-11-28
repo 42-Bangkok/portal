89b053170efdb98e25148a14009e15dfef46abb6
Pumidol Leelerdsakulvong <pumidol.l@gmail.com>
2023-11-28 12:33:49 +0000

    Add MongoDB connection and database function

---
+++ app/lib/db/db.ts
@@ -0,0 +1,19 @@
+import { Db, MongoClient } from 'mongodb';
+
+/**
+ * Returns a connected MongoDB client.
+ *
+ * @returns {Promise<Db>} A promise that resolves to a MongoDB client.
+ */
+export async function getDb(): Promise<Db> {
+  try {
+    const uri = process.env['MONGODB_URI'] ?? 'mongodb://localhost:27017';
+    const client = new MongoClient(uri);
+    await client.connect();
+    const database = client.db(process.env['MONGODB_DB'] ?? 'mongo');
+    return database;
+  } catch (error) {
+    console.error('Error connecting to database:', error);
+    throw error;
+  }
+}
