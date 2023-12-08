f0cef2401fd3a38b2340323eb1fc8b09d53447dd
Pumidol Leelerdsakulvong <pumidol.l@gmail.com>
2023-12-08 07:34:03 +0000

    Refactor NewMarkerDialog component to use react-hook-form

--- app/app/(appmap)/_components/map-dashboard/new-marker-dialog.component.tsx
+++ app/app/(appmap)/_components/map-dashboard/new-marker-dialog.component.tsx
@@ -10,17 +10,47 @@ import {
   DialogTitle,
   DialogTrigger,
 } from "@/components/ui/dialog";
+import {
+  Form,
+  FormControl,
+  FormDescription,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form";
 import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
 import { useMapStore } from "../map/stores";
 import { createMarker } from "@/lib/db/appmap/markers";
 import { toast } from "sonner";
+import { useState } from "react";
+import { z } from "zod";
+import { useForm } from "react-hook-form";
+import { zodResolver } from "@hookform/resolvers/zod";
 
+const FormSchema = z.object({
+  title: z.string().min(5, { message: "Title is required" }),
+  description: z.string().min(5, { message: "Description is required" }),
+});
+
+/**
+ * Dialog to create a new marker
+ */
 export function NewMarkerDialog() {
   const [position] = useMapStore((state) => [state.position]);
+  const [open, setOpen] = useState(false);
+  const [pending, setPending] = useState(false);
+  const form = useForm<z.infer<typeof FormSchema>>({
+    resolver: zodResolver(FormSchema),
+    defaultValues: {
+      title: "",
+      description: "",
+    },
+  });
+
   const handleSubmit = async (e: any) => {
-    e.preventDefault();
-    console.log(position);
+    setPending(true);
     const { data, error } = await createMarker({
       title: "test",
       description: "test",
@@ -31,12 +61,16 @@ export function NewMarkerDialog() {
     });
     if (data) {
       toast.success("Successfully created a new marker!");
+      setOpen(false);
+      setPending(false);
     } else if (error) {
+      setPending(false);
       toast.error(error);
     }
   };
+
   return (
-    <Dialog>
+    <Dialog open={open} onOpenChange={setOpen}>
       <DialogTrigger asChild>
         <Button>New Marker</Button>
       </DialogTrigger>
@@ -47,33 +81,50 @@ export function NewMarkerDialog() {
             This will add a new marker in the center of your current map.
           </DialogDescription>
         </DialogHeader>
-        <div className="grid gap-4 py-4">
-          <div className="grid grid-cols-4 items-center gap-4">
-            <Label htmlFor="name" className="text-right">
-              Title
-            </Label>
-            <Input
-              id="title"
-              placeholder="title of your marker"
-              className="col-span-3"
+        <Form {...form}>
+          <form
+            className="grid gap-4 py-4"
+            onSubmit={form.handleSubmit(handleSubmit)}
+          >
+            <FormField
+              control={form.control}
+              name="title"
+              render={({ field }) => (
+                <FormItem className="grid grid-cols-4 items-center gap-4">
+                  <FormLabel className="text-right">Title</FormLabel>
+                  <FormControl>
+                    <Input
+                      placeholder="title of your marker"
+                      className="col-span-3"
+                      {...field}
+                    />
+                  </FormControl>
+                </FormItem>
+              )}
             />
-          </div>
-          <div className="grid grid-cols-4 items-center gap-4">
-            <Label htmlFor="username" className="text-right">
-              Description
-            </Label>
-            <Input
-              id="description"
-              placeholder="short description of your marker"
-              className="col-span-3"
+            <FormField
+              control={form.control}
+              name="description"
+              render={({ field }) => (
+                <FormItem className="grid grid-cols-4 items-center gap-4">
+                  <FormLabel className="text-right">Description</FormLabel>
+                  <FormControl>
+                    <Input
+                      placeholder="short description of your marker"
+                      className="col-span-3"
+                      {...field}
+                    />
+                  </FormControl>
+                </FormItem>
+              )}
             />
-          </div>
-        </div>
-        <DialogFooter>
-          <Button type="submit" onClick={handleSubmit}>
-            Save changes
-          </Button>
-        </DialogFooter>
+            <DialogFooter>
+              <Button type="submit" disabled={pending}>
+                Save changes
+              </Button>
+            </DialogFooter>
+          </form>
+        </Form>
       </DialogContent>
     </Dialog>
   );
