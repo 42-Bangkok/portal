a0ab4908f85867104c2dfa4e5d927517462bbb6a
Pumidol Leelerdsakulvong <pumidol.l@gmail.com>
2023-12-12 07:10:14 +0000

    Refactor marker creation and retrieval functions

--- app/lib/db/appmap/markers.ts
+++ app/lib/db/appmap/markers.ts
@@ -1,13 +1,71 @@
 "use server";
 
-import { SVAResponse } from "../types";
-import { TNewMarker, CreateMarkerSchema } from "./schemas";
+import { getServerSession } from "next-auth";
+import { SAResponse } from "../types";
+import {
+  CreateMarkerSchema,
+  MarkerSchema,
+  TCreateMarker,
+  TMarker,
+} from "./schemas";
+import { authOptions } from "@/lib/auth/auth-options";
+import { getDb } from "../db";
+import { revalidatePath } from "next/cache";
 
 export async function createMarker(
-  data: TNewMarker
-): Promise<SVAResponse<boolean>> {
-  const parsed = CreateMarkerSchema.parse(data);
-  console.log(parsed);
+  data: TCreateMarker
+): Promise<SAResponse<boolean>> {
+  const parsed = CreateMarkerSchema.safeParse(data);
+  if (!parsed.success) {
+    return { data: null, error: "invalid data" };
+  }
+  const session = await getServerSession(authOptions);
+  if (!session) {
+    return { data: null, error: "not authenticated" };
+  }
+  const payload = {
+    title: parsed.data.title,
+    description: parsed.data.description,
+    position: {
+      type: "Point",
+      coordinates: [parsed.data.lng, parsed.data.lat],
+    },
+    createdBy: session.user.login,
+    updatedBy: session.user.login,
+    createdAt: new Date().toISOString(),
+    updatedAt: new Date().toISOString(),
+  };
+  const db = await getDb();
+  const res = await db.collection("markers").insertOne(payload);
+  if (!res.acknowledged) {
+    return { data: null, error: "failed to create marker" };
+  }
+  revalidatePath("/map");
   return { data: true, error: null };
-  return { data: null, error: "test error" };
+}
+
+export async function getMarkers(amt: number): Promise<TMarker[]> {
+  const db = await getDb();
+  const res = await db
+    .collection("markers")
+    .find()
+    .sort({ createdAt: -1 })
+    .limit(amt)
+    .toArray();
+  const markers = res.map((marker) => {
+    const _ = MarkerSchema.parse({
+      id: marker._id.toHexString(),
+      title: marker.title,
+      description: marker.description,
+      lat: marker.position.coordinates[1] as number,
+      lng: marker.position.coordinates[0] as number,
+      featured: marker.featured,
+      createdBy: marker.createdBy,
+      updatedBy: marker.updatedBy,
+      createdAt: marker.createdAt,
+      updatedAt: marker.updatedAt,
+    });
+    return _;
+  });
+  return markers;
 }
