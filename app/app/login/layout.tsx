433b195d1af29cbf81694dba677c1abd0036d583
Pumidol Leelerdsakulvong <pumidol.l@gmail.com>
2023-12-12 09:16:41 +0000

    Add login layout component

---
+++ app/app/login/layout.tsx
@@ -0,0 +1,18 @@
+import "@/styles/globals.css";
+
+export const metadata = {
+  title: "42 Bangkok's Portal",
+  description: "Login to 42 Bangkok's Portal",
+};
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <html lang="en">
+      <body>{children}</body>
+    </html>
+  );
+}
