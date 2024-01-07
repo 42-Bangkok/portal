989616bcaa4acf235f40aade163425d8af2968dd
Pumidol Leelerdsakulvong <pumidol.l@gmail.com>
2024-01-07 20:09:07 +0700

    Feat/next auth 5 (#24)
    * move providers to root
    
    * Update next-auth to version 5.0.0-beta.4
    
    * migrate to next auth 5
    
    * rename ENVs as per v5 docs

--- app/app/(appmap)/layout.tsx
+++ app/app/(appmap)/layout.tsx
@@ -1,7 +1,7 @@
 import "@/styles/globals.css";
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
-import { Providers } from "@/app/providers";
+import { Providers } from "@/providers";
 import { Toaster } from "sonner";
 
 const inter = Inter({ subsets: ["latin"] });
--- app/app/(root)/layout.tsx
+++ app/app/(root)/layout.tsx
@@ -1,20 +1,20 @@
-import { NavBar } from '@/components/nav-bar'
-import '@/styles/globals.css'
-import type { Metadata } from 'next'
-import { Inter } from 'next/font/google'
-import { Providers } from '../providers'
+import { NavBar } from "@/components/nav-bar";
+import "@/styles/globals.css";
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
+import { Providers } from "../../providers";
 
-const inter = Inter({ subsets: ['latin'] })
+const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: 'Portal',
-  description: 'Portal',
-}
+  title: "Portal",
+  description: "Portal",
+};
 
 export default function RootLayout({
   children,
 }: {
-  children: React.ReactNode
+  children: React.ReactNode;
 }) {
   return (
     <html lang="en">
@@ -27,5 +27,5 @@ export default function RootLayout({
         </Providers>
       </body>
     </html>
-  )
+  );
 }
