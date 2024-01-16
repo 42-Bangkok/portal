c98293e8b62077169e3320c1637c831e86f25d44
Pumidol Leelerdsakulvong <pumidol.l@gmail.com>
2024-01-16 11:27:05 +0700

    Update app/app/(root)/profile/setting/error.tsx
    Co-authored-by: hazamashoken <46592735+hazamashoken@users.noreply.github.com>

--- app/app/(root)/profile/setting/error.tsx
+++ app/app/(root)/profile/setting/error.tsx
@@ -1,5 +1,16 @@
-"use client";
-
-export default function Error() {
-  return <div>Something went wrong</div>;
+'use client'
+ 
+export default function Error({
+  error,
+  reset,
+}: {
+  error: Error & { digest?: string }
+  reset: () => void
+}) {
+  return (
+    <div>
+      <h2>Something went wrong!</h2>
+      <button onClick={() => reset()}>Try again</button>
+    </div>
+  )
 }
