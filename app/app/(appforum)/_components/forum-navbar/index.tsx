7757e934cf684ac196af49ea3954b7048a4c55ee
tliangso <earth78203@gmail.com>
2024-01-19 14:03:48 +0700

    add version indicator

--- app/app/(appforum)/_components/forum-navbar/index.tsx
+++ app/app/(appforum)/_components/forum-navbar/index.tsx
@@ -2,6 +2,7 @@ import Link from "next/link";
 import { ForumSearchBar } from "./forum-searchbar";
 import { Button } from "@/components/ui/button";
 import { HomeIcon, List } from "lucide-react";
+import { APPFORUM_VERSION } from "@/version";
 
 export const ForumNavBar = () => {
   return (
@@ -22,6 +23,9 @@ export const ForumNavBar = () => {
       <Link href="/forum/create">
         <Button>Create Post</Button>
       </Link>
+      <p className="fixed bottom-0 right-0 p-1 text-xs text-slate-500">
+        v{APPFORUM_VERSION}
+      </p>
     </div>
   );
 };
---
+++ app/version.ts
@@ -0,0 +1,5 @@
+/**
+ * Optional Versioning for your app
+ */
+
+export const APPFORUM_VERSION = "0.1.0";
