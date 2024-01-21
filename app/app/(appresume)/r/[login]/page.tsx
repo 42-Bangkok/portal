53614fe55f465a4bb536136a22979db2e6081ae8
Pumidol Leelerdsakulvong <pumidol.l@gmail.com>
2024-01-21 19:19:35 +0000

    Add TODO for resume-ii and allow user to edit additional information

--- app/app/(appresume)/r/[login]/page.tsx
+++ app/app/(appresume)/r/[login]/page.tsx
@@ -2,6 +2,8 @@
  * Public resume page
  * if user allows public resume it is available at /r/[login]
  * else display 404
+ * TODO: resume-ii
+ * if current user is the same as [login], allow the user to edit additional information
  */
 export default function Page() {
   return null;
